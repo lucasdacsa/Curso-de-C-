39de12b06045fc0a4be32b71a02b7ee7f9806b46
lucasdacsa <75916029+lucasdacsa@users.noreply.github.com>
2025-07-29 10:44:21 -0300

    Add files via upload

---
+++ Aula 01/primeiro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main (){
+//o comando /n pula a linha, std::edl1; embas pulam a linha
+    std::cout << "hell world!\n";
+    std::cout << ", Heloooo" << std::endl;
+    std::cout << "oxetec lab";
+    return 0;
+    
+}
